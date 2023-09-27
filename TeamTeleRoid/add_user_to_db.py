350348a69f6a6115fdafa55cbdad370ab1b36958
Preet mehra <143023174+ROYAL-JATT@users.noreply.github.com>
2023-09-27 13:13:33 +0530

    Update add_user_to_db.py

--- TeamTeleRoid/add_user_to_db.py
+++ TeamTeleRoid/add_user_to_db.py
@@ -1,14 +1,28 @@
+from pyrogram import Client, filters
+import datetime
 from configs import Config
-from handlers.database import db
-from pyrogram import Client
+from TeamTeleRoid.database import db
 from pyrogram.types import Message
 
+@Client.on_message(filters.private)
+async def handle_user_status(bot:Client, cmd:Message):
+    chat_id = cmd.from_user.id
 
-async def AddUserToDatabase(bot: Client, cmd: Message):
-    if not await db.is_user_exist(cmd.from_user.id):
-        await db.add_user(cmd.from_user.id)
-        if Config.LOG_CHANNEL is not None:
-            await bot.send_message(
-                int(Config.LOG_CHANNEL),
-                f"#NEW_USER: \n\nNew User [{cmd.from_user.first_name}](tg://user?id={cmd.from_user.id}) started @{Config.BOT_USERNAME} !!"
-            )
+    if not await db.is_user_exist(chat_id):
+        print("True")
+        await db.add_user(chat_id)
+        await bot.send_message(
+            Config.LOG_CHANNEL,
+            f"#NEW_USER: \n\nNew User [{cmd.from_user.first_name}](tg://user?id={cmd.from_user.id}) started @{Config.BOT_USERNAME} !!"
+        )
+
+    ban_status = await db.get_ban_status(chat_id)
+    if ban_status["is_banned"]:
+        if (
+                datetime.date.today() - datetime.date.fromisoformat(ban_status["banned_on"])
+        ).days > ban_status["ban_duration"]:
+            await db.remove_ban(chat_id)
+        else:
+            await cmd.reply_text("You Are Ban To Use This Bot.😜", quote=True)
+            return
+    await cmd.continue_propagation()
