e2a4e4b0e90a36ef69ecfd3e9fb7812345f8f172
Preet mehra <143023174+ROYAL-JATT@users.noreply.github.com>
2024-02-20 08:21:38 +0530

    Update main.py

--- main.py
+++ main.py
@@ -9,7 +9,7 @@ from pyrogram import Client
 
 # Bot Client for Inline Search
 Bot = Client(
-    session_string=Config.USER_SESSION_STRING	
+    session_string=Config.USER_SESSION_STRING,	
     api_id=Config.API_ID,
     api_hash=Config.API_HASH,
     bot_token=Config.BOT_TOKEN
